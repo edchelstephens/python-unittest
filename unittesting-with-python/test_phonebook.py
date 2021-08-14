7360eaaf7a09471ce71560e0edc9634bb6910244
Edchel Stephen <edchelstephens@gmail.com>
2021-08-14 15:00:43 +0800

    Testcase for phonebook module.

---
+++ unittesting-with-python/test_phonebook.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+from phonebook import PhoneBook
+
+class PhoneBookTestCase(TestCase):
+    """Test case for Phonebook class."""
+
+    def test_lookup_by_name(self):
+        """Phonebook lookup by name returns contact if present."""
+        
+        phonebook = PhoneBook()
+        
+        name = "Ed"
+        number = "123456"
+        phonebook.add(name, number)
+        
+        
+        lookup_number = phonebook.lookup(name)
+
+        self.assertEqual(lookup_number, number)
