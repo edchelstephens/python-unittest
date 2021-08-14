53c9b384e3506d992cd45bc1daffae11c296325a
Edchel Stephen <edchelstephens@gmail.com>
2021-08-14 15:43:48 +0800

    use setUp() to remove redundancy in creating new phonebook each test case.

--- unittesting-with-python/test_phonebook.py
+++ unittesting-with-python/test_phonebook.py
@@ -6,31 +6,30 @@ from phonebook import PhoneBook
 class PhoneBookTestCase(TestCase):
     """Test case for Phonebook class."""
 
+    def setUp(self) -> None:
+        """Run setUp for each test to create a fresh phonebook."""
+        self.phonebook = PhoneBook()
+
     def test_lookup_by_name(self):
         """Phonebook lookup by name returns contact if present."""
         
-        phonebook = PhoneBook()
-        
         name = "Ed"
         number = "123456"
-        phonebook.add(name, number)
+
+        self.phonebook.add(name, number)
         
-        lookup_number = phonebook.lookup(name)
+        lookup_number = self.phonebook.lookup(name)
 
         self.assertEqual(lookup_number, number)
 
-    def test_missing_name(self):
+    def test_lookup_missing_name_raises_KeyError(self):
         """Not found phonebook name raises KeyError"""
 
-        phonebook = PhoneBook()
-
         with self.assertRaises(KeyError):
-            phonebook.lookup("not there")  
+            self.phonebook.lookup("not there")  
     
     @skip("INCOMPLETE")
     def test_empty_phonebook_is_consistent(self):
         """Test phonebook consistency on unqiue name to number recording."""
-        
-        phonebook = PhoneBook()
 
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
