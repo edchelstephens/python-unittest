424cfb4ebceae28fd6e54458aa7efa8d68197d18
Edchel Stephen <edchelstephens@gmail.com>
2021-08-14 14:35:42 +0800

    unit testing with python study package after course on pluralsight by Emily Bache - Unit Testing with Python.

---
+++ unittesting-with-python/__init__.py
@@ -0,0 +1,6 @@
+"""Unit testing with python coding as following Plurasight course*
+
+* Unit Testing with Python
+https://app.pluralsight.com/library/courses/ec92942a-62c7-466e-ba92-56201eaf900f/table-of-contents
+By Emily Bache
+"""
