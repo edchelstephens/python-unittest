c6785a9ed3f1e4f4da3d8f62073d4465fafc3c3e
Edchel Stephen <edchelstephens@gmail.com>
2021-08-14 15:00:17 +0800

    phonebook module for Phonebook class.

---
+++ unittesting-with-python/phonebook.py
@@ -0,0 +1,16 @@
+"""Phonebook module."""
+
+class PhoneBook:
+    """Phonebook class."""
+
+    def __init__(self) -> None:
+        """Initialize phonebook with a phonebook dictionary."""
+        self.phonebook = {}
+
+    def add(self, name:str, number:str) -> None:
+        """Add name and number to phonebook"""
+        self.phonebook[name] = number
+
+    def lookup(self, name:str) -> str:
+        """Return phonebook number for name."""
+        return self.phonebook.get(name)
