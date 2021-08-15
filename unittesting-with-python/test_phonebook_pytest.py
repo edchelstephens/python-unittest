969cd88ab35ce53c6884c3f36ab6dbe51719bb07
Edchel Stephen <edchelstephens@gmail.com>
2021-08-15 16:32:25 +0800

    test missing lookup raises KeyError.

--- unittesting-with-python/test_phonebook_pytest.py
+++ unittesting-with-python/test_phonebook_pytest.py
@@ -1,7 +1,10 @@
+import pytest
+
 from phonebook import PhoneBook
 
 def test_lookup_by_name():
     """Name lookup returns number on phonebook when present."""
+    
     phonebook = PhoneBook()
     phonebook.add("Ed", "123")
     
@@ -14,3 +17,12 @@ def test_phonebook_contains_all_names():
     phonebook.add("Ed", 123)
 
     assert "Ed" in phonebook.get_names()
+
+
+def test_missing_phonebook_lookup_raises_error():
+    """missing name lookup on phonebook raises KeyError"""
+
+    phonebook = PhoneBook()
+
+    with pytest.raises(KeyError):
+        phonebook.lookup("NotThere")
